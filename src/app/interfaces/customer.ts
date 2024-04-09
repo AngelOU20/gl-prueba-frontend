472f870905d0bf153c89504d96f7a749d92abace
Julio Ucharima <julio_ucharima@usmp.pe>
2024-04-09 11:32:00 -0500

    feat: add customer interface

---
+++ src/app/interfaces/customer.ts
@@ -0,0 +1,12 @@
+export interface Customer {
+  id?: number;
+  name: string;
+  email: string;
+  phoneNumber: string;
+  address: string;
+  city: string;
+  country: string;
+  isActive: boolean;
+  createdAt: Date;
+  updatedAt: Date;
+}
