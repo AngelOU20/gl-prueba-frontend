87cdc06ff971760c0fbeaedd0cf91fc02a0f85b6
Julio Ucharima <julio_ucharima@usmp.pe>
2024-04-10 16:19:17 -0500

    feat: integrate SharedModule and new components in AppModule

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,13 +4,27 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 
+// Modules
+import { SharedModule } from './shared/shared.module';
+
+// Components
+import { AddEditCustomerComponent } from './components/add-edit-customer/add-edit-customer.component';
+import { ListCustomerComponent } from './components/list-customer/list-customer.component';
+import { DetailCustomerComponent } from './components/detail-customer/detail-customer.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    AddEditCustomerComponent,
+    ListCustomerComponent,
+    DetailCustomerComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    BrowserAnimationsModule,
+    SharedModule,
   ],
   providers: [],
   bootstrap: [AppComponent]
