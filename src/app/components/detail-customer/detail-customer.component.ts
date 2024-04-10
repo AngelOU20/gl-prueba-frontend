4524e1090b18c5dda8adea6386312b90906e8bb1
Julio Ucharima <julio_ucharima@usmp.pe>
2024-04-10 16:30:58 -0500

    feat: implement customer detail component logic

--- src/app/components/detail-customer/detail-customer.component.ts
+++ src/app/components/detail-customer/detail-customer.component.ts
@@ -1,4 +1,7 @@
 import { Component } from '@angular/core';
+import { ActivatedRoute } from '@angular/router';
+import { Customer } from 'src/app/interfaces/customer';
+import { CustomerService } from 'src/app/services/customer.service';
 
 @Component({
   selector: 'app-detail-customer',
@@ -6,5 +9,24 @@ import { Component } from '@angular/core';
   styleUrls: ['./detail-customer.component.css']
 })
 export class DetailCustomerComponent {
+  id: number;
+  customer!: Customer;
+  isLoading: boolean = false;
 
+  constructor (private _customerService: CustomerService, private aRoute: ActivatedRoute) {
+    this.id = parseInt(this.aRoute.snapshot.paramMap.get('id')!);
+
+  }
+
+  ngOnInit (): void {
+    this.getCustomerById();
+  }
+
+  getCustomerById () {
+    this.isLoading = true;
+    this._customerService.getCustomerById(this.id).subscribe(data => {
+      this.isLoading = false;
+      this.customer = data;
+    });
+  }
 }
