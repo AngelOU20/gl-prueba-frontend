4dc2d2cd3cf4abd16c4d4a605f09724e3777260a
Julio Ucharima <julio_ucharima@usmp.pe>
2024-04-10 16:23:09 -0500

    feat: add routing to AppRoutingModule

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,29 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 
-const routes: Routes = [];
+// Components
+import { ListCustomerComponent } from './components/list-customer/list-customer.component';
+import { AddEditCustomerComponent } from './components/add-edit-customer/add-edit-customer.component';
+import { DetailCustomerComponent } from './components/detail-customer/detail-customer.component';
+
+const routes: Routes = [
+  { path: '', redirectTo: 'list-customers', pathMatch: 'full' },
+  {
+    path: 'list-customers', component: ListCustomerComponent
+  },
+  {
+    path: 'add-customer', component: AddEditCustomerComponent
+  },
+  {
+    path: 'view-detail-customer/:id', component: DetailCustomerComponent
+  },
+  {
+    path: 'edit-customer/:id', component: AddEditCustomerComponent
+  },
+  {
+    path: '**', redirectTo: 'list-customers', pathMatch: 'full'
+  }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
